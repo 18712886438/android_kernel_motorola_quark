7cb45fe1aa4dbf88fc45ecfe45898b77d8a33e58
CallMESuper <zj7859256@gmail.com>
2016-06-10 23:17:55 +0800

    add a simple build script for now

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export CONFIG_FILE="apq8084-perf_defconfig"
+export ARCH="arm"
+export CROSS_COMPILE="arm-eabi-"
+export TOOL_CHAIN_PATH="${HOME}/Mokee/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin"
+export CONFIG_ABS_PATH="arch/${ARCH}/configs/${CONFIG_FILE}"
+export PATH=$PATH:${TOOL_CHAIN_PATH}
+make -j4 $CONFIG_FILE
+make -j4
