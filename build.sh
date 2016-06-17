b1e9e0172506ce4299b5d58d392be54d78d5cc3f
CallMESuper <zj7859256@gmail.com>
2016-06-17 22:38:34 +0800

    update build script

--- build.sh
+++ build.sh
@@ -10,3 +10,7 @@ make -j4 $CONFIG_FILE
 make -j4
 mkdir modules
 find . -name '*.ko' -exec cp -av {} modules/ \;
+# strip modules 
+${TOOL_CHAIN_PATH}/${CROSS_COMPILE}strip --strip-unneeded modules/*
+mkdir modules/qca_cld
+mv modules/wlan.ko modules/qca_cld/qca_cld_wlan.ko
