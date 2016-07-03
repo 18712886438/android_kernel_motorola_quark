45236fc71d7954bc4b022e805441d0a0d25c86fa
CallMESuper <zj7859256@gmail.com>
2016-07-04 01:38:23 +0800

    build.sh add dt build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -rf modules
+rm -rf modules && rm /arch/arm/boot/dt.img
 export CONFIG_FILE="apq8084-perf_defconfig"
 export ARCH="arm"
 export CROSS_COMPILE="arm-eabi-"
@@ -14,3 +14,5 @@ find . -name '*.ko' -exec cp -av {} modules/ \;
 ${TOOL_CHAIN_PATH}/${CROSS_COMPILE}strip --strip-unneeded modules/*
 mkdir modules/qca_cld
 mv modules/wlan.ko modules/qca_cld/qca_cld_wlan.ko
+./tools/dtbToolCM -2 -o ./arch/arm/boot/dt.img -s 4096 -p .scripts/dtc/ ./arch/arm/boot/dts/
+lz4 -9 ./arch/arm/boot/dt.img
